aabfe7de508ed8fd7f5e6e35f3e298d7136b2f6b
zyq0330 <907769974@qq.com>
2026-07-14 14:51:05 +0800

    我的第一个c++词频程

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+#include<map>
+#include<sstream>
+#include<string>
+using namespace std;
+int main() {
+  ifstream file("input.txt");
+    if (!file.is_open()) {
+        cerr<<"err，notfind input.txt"<<endl;
+        return 1;
+    }
+    string line;
+    string word;
+    map<string,int>wordCount;
+     while (getline(file,line)) {
+        stringstream ss(line);
+        while (ss>>word) {
+            wordCount[word]++;
+        }
+    }
+    file.close();
+    cout<<"words rate:"<<endl;
+    for (const auto &pair:wordCount) {
+        cout<<pair.first<<":"<<pair.second<<endl;
+    }
+    return 0;
+}
